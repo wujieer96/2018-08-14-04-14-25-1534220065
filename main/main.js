567ed2e39ba56100f866bea51b8d6f55083a3f2a
wujieer96 <895326733@qq.com>
2019-05-28 21:03:43 +0800

    LCD

--- main/main.js
+++ main/main.js
@@ -1,4 +1,11 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(str) {
+    if str=="910"{
+	console.log("._. ... ._.\n"+"|_| ..| |.|\n"+"..| ..| |_|\n");
+}
+    else if str=="256"{
+	console.log("._. ._. ._.\n"+"._| |_. |_.\n"+"|_. ._| |_|\n");
+}
+    else if str=="7"{
+	console.log("._.\n"+"..|\n"+"..|\n");
+}
 };
