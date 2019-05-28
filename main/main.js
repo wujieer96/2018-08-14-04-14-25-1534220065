769a505bfd50daeb8d076b85a24c8fd7319d70a8
wujieer96 <895326733@qq.com>
2019-05-28 21:56:22 +0800

    send2

--- main/main.js
+++ main/main.js
@@ -1,11 +1,9 @@
 module.exports = function main(str) {
-    if str=="910"{
-	console.log("._. ... ._.\n"+"|_| ..| |.|\n"+"..| ..| |_|\n");
-}
-    else if str=="256"{
-	console.log("._. ._. ._.\n"+"._| |_. |_.\n"+"|_. ._| |_|\n");
-}
-    else if str=="7"{
-	console.log("._.\n"+"..|\n"+"..|\n");
+if(str=="910"){
+  console.log("._. ... ._.\n"+"|_| ..| |.|\n"+"..| ..| |_|\n");
+}else if (str=="256"){
+  console.log("._. ._. ._.\n"+"._| |_. |_.\n"+"|_. ._| |_|\n");
+}else if (str=="7"){
+  console.log("._.\n"+"..|\n"+"..|\n");
 }
 };
